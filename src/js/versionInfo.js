fa4110002468046e648981577e6f233fadc9479a
Jason Kao <jasonkao85@gmail.com>
2018-01-28 09:59:44 -0500

    fixed npm security vulnerability

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.293' };
+let VersionInfo = { VERSION : '0.0.294' };
 export default VersionInfo;
