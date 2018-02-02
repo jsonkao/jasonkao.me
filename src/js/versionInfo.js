4981e4ac9f72abb8e0c58d97a869a185ff400383
Jason Kao <jasonkao85@gmail.com>
2018-02-02 07:02:19 -0500

    citidots

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.449' };
+let VersionInfo = { VERSION : '0.0.450' };
 export default VersionInfo;
