1f28551c4abd7a019ec465bd2e146b6e2b8395df
Jason Kao <jasonkao85@gmail.com>
2018-02-04 10:25:26 -0500

    buzzwords, vinfo

--- src/js/modules/core/components/MainApp.js
+++ src/js/modules/core/components/MainApp.js
@@ -122,7 +122,7 @@ class MainApp extends PureComponent {
         <div className={classes.contentSection}>
           <p>Buzzwords&mdash;</p>
           <p className={classes.subText}>
-            JavaScript (ES7), Python, Java, Ruby, C, SQL
+            JavaScript (ES7), Python, Java, Ruby, HTML, CSS, C, SQL
           </p>
           <p className={classes.subText}>
             React / Redux / Apollo, Rails, GraphQL, Docker, Git, Node, MongoDB,
