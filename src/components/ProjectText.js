5bb2e09652aeb81877ae2a17afdeab52ed2d7639
Jason Kao <jason.kao@columbia.edu>
2020-05-06 11:49:47 -0400

    copied in old components

---
+++ src/components/ProjectText.js
@@ -0,0 +1,63 @@
+import React, { PureComponent } from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+  ProjectText: {
+    clear: 'both',
+    display: 'block',
+    margin: '4px auto',
+    '& a': {},
+    '&:before': {
+      content: '" "',
+      display: 'table',
+    },
+    '&:after': {
+      clear: 'both',
+      content: '" "',
+      display: 'table',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+      '& a': {
+        color: '#000',
+      },
+    },
+  },
+  link: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: '#777',
+    height: '100%',
+    textDecoration: 'none',
+    width: '100%',
+    '&:hover': {
+      color: '#000',
+    },
+  },
+  noticeMe: {
+    color: '#f00',
+  },
+  arrow: {
+    alignSelf: 'flex-end',
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif',
+  },
+};
+
+const ProjectText = ({ classes, project, showProject }) => {
+  return (
+    <li className={classes.ItemText} onMouseEnter={() => showProject(project)}>
+      <a href={project.url} target="_blank" className={classes.link}>
+        <div>
+          {project.title}
+          {project.noticeMe && (
+            <span className={classes.noticeMe}> ({project.noticeMe})</span>
+          )}
+        </div>
+        <div className={classes.arrow}>↗</div>
+      </a>
+    </li>
+  );
+};
+
+export default injectSheet(styles)(ProjectText);
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as PageLayout } from './PageLayout';
+export { default as MainApp } from './MainApp';
+export { default as FixedNavbar } from './FixedNavbar';
+export { default as Navbar } from './Navbar';
+export { default as CelestialCircle } from './CelestialCircle';
+export { default as List } from './List';
