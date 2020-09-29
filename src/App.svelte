cf6bc40c8f81f8d7b34f669c5c468e569924c5a4
Jason Kao <jason.kao@columbia.edu>
2020-09-29 14:22:45 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -196,7 +196,7 @@
 
     <SectionHeader empty />
     <p class="updated-text">Updated August 2020. <span style="text-transform: none;">
-      All are welcome to use this code/design under the <a
+      All are welcome to use this <a href="https://github.com/jsonkao/jasonkao.me">code/design</a> under the <a
         rel="license"
         href="http://creativecommons.org/licenses/by-sa/4.0/"
       >Creative Commons Attribution-ShareAlike 4.0 International License</a>.</span>
