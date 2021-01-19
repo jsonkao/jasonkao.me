614fc566d2f2cb2785619f2cc1e876fedabd843b
Jason Kao <jason.kao@columbia.edu>
2021-01-19 10:49:12 -0500

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ export default {
     {
       name: 'Low-income, Black, Hispanic Communities Rebounded Weeks Faster in Movement',
       image: 'images/nyc-reopen.webp',
-      description: '“It’s very clear that social distancing is a privilege that not everyone can afford."',
+      description: '“It’s very clear that social distancing is a privilege that not everyone can afford.”',
       url: 'https://www.bloomberg.com/graphics/2020-new-york-city-reopening/',
     },
     {
