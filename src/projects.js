25322d405711846069f64634df93330ab1592666
Jason Kao <jason.kao@columbia.edu>
2021-02-14 20:58:57 -0500

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -5,6 +5,7 @@ export default {
       image: 'images/nyc-reopen.webp',
       description: '“It’s very clear that social distancing is a privilege that not everyone can afford.”',
       url: 'https://www.bloomberg.com/graphics/2020-new-york-city-reopening/',
+      people: 'Andre Tartar, Paul Murray, Jeremy Lin, Elena Popina, Eugene Reznik, Yue Qiu, Alex Tribou, Makada Henry-Nickie'
     },
     {
       name:
@@ -14,6 +15,7 @@ export default {
         'A new analysis shows FEMA flood zone maps underestimate properties in danger by 67%.',
       url:
         'https://www.bloomberg.com/graphics/2020-flood-risk-zone-us-map/',
+      people: 'Mira Rojanasakul, Hayley Warren',
     },
     {
       name:
@@ -23,6 +25,7 @@ export default {
         'Investigating and mapping where children were held. Model by <a href="http://blueshirt.com">Jeremy White</a>.',
       url:
         'https://www.nytimes.com/interactive/2019/07/06/us/migrants-border-patrol-clint.html',
+      people: 'Caitlin Dickerson, Simon Romero, Zolan Kanno-Youngs, Manny Fernandez, Simone Landon, Juliette Love, Jeremy White, Jugal Patel'
     },
     {
       name:
@@ -32,6 +35,7 @@ export default {
         'Multimedia maps depict the scale of 2019 protests in Hong Kong. Conceived by <a href="https://twitter.com/jwf825">Jin Wu</a>.',
       url:
         'https://www.nytimes.com/interactive/2019/06/20/world/asia/hong-kong-protest-size.html',
+      people: 'Jin Wu, Anjali Singhvi, Archie Tse'
     },
     {
       name:
@@ -41,12 +45,14 @@ export default {
         'Visualizations explaining the impact of the metering and Remain in Mexico programs.',
       url:
         'https://www.nytimes.com/interactive/2019/08/18/us/mexico-immigration-asylum.html',
+      people: 'Simone Landon, Denise Lu, Archie Tse, Zolan Kanno-Youngs, Stephanie Leutert'
     },
     {
       name: 'U.S. and Ivy League schools were late to respond to COVID-19',
       image: 'images/ivy-response.mp4',
       url:
         'https://www.columbiaspectator.com/news/2020/04/13/the-us-and-ivy-league-schools-were-late-to-respond-to-covid-19-data-shows-international-universities-did-better',
+      people: 'Kelly Pu, Jun Yi Zhang, Stephanie Lai, Raeedah Wahid, Hong Sen Du, Karen Xia'
     },
     {
       name: 'Uncertainty Looms as Student Jobs Disappear',
@@ -55,6 +61,7 @@ export default {
         'Analyzing the industries losing jobs during the coronavirus recession.',
       url:
         'https://www.columbiaspectator.com/eye-lead/2020/05/15/uncertainty-looms-as-student-jobs-are-disappearing-during-the-great-lockdown',
+      people: 'Eve Washington, Charlotte Li, Chaitanya Reddy Gurijala'
     },
     {
       name: 'Broken windows policing & gentrification harmed Black community, strengthened Columbia',
@@ -62,6 +69,7 @@ export default {
       description: 'Scraped and processed all NYC deeds since 1966 to understand Columbia’s expansion.',
       url:
         'https://www.columbiaspectator.com/news/2020/10/05/broken-windows-policing-and-gentrification-not-only-harmed-the-black-community-but-also-strengthened-columbias-reputation/',
+      people: 'Sofia Kwon, Stephanie Lai'
     },
     {
       name: 'Gerrymandering playground (2018)',
@@ -69,6 +77,7 @@ export default {
       description:
         'Interactive mapping as an educational tool for gerrymandering congressional districts.',
       url: 'https://github.com/gerry-app',
+      people: 'Patrick Chan, Billy Wong'
     },
   ],
   secondLevel: [
@@ -76,16 +85,19 @@ export default {
       name: 'More at <code>nytimes.com/by/jason-kao</code>',
       image: 'images/nyt-byline-refer.webp',
       url: 'https://www.nytimes.com/by/jason-kao',
+      people: 'Simone Landon, Archie Tse, Tom Giratikanon, Joe Ward, Denise Lu, K.K. Rebecca Lai, Jugal Patel',
     },
     {
       name: 'More at <code>bloomberg.com/authors/jason-kao</code>',
       image: 'images/bloomberg-byline-refer.webp',
       url: 'https://www.bloomberg.com/authors/AUy6CJsqwxM/jason-kao',
+      people: 'Yue Qiu, Alex Tribou',
     },
     {
       name: 'More of our Spec work at <code>graphicsdesk.github.io</code>',
       image: 'images/spec.webp',
       url: 'https://graphicsdesk.github.io',
+      people: 'Raeedah Wahid, Charlotte Li, Jun Yi Zhang, Karen Xia',
     },
     {
       name: 'The Stuyvesant Spectator (high school paper)',
@@ -93,6 +105,7 @@ export default {
       description:
         'Led the creation of a React site, Postgres database, Rails/GraphQL API, and Go CMS.',
       url: 'https://github.com/stuyspec',
+      people: 'Nicholas Yang, Darius Jankauskas, Jason Lin, Cathy Cai',
     },
     {
       name: 'Moral Foundations in Political Debates (2018)',
@@ -104,6 +117,7 @@ export default {
       image: 'images/graduate-sex-diversity.mp4',
       url:
         'https://www.columbiaspectator.com/eye-lead/2019/05/04/in-certain-science-and-engineering-fields-sex-diversity-among-graduate-students-is-stagnating-in-others-its-getting-worse',
+      people: 'Gavrielle Jacobovitz, Julian Shen-Berro, Rahil Kamath'
     },
   ],
   openSource: [
@@ -113,6 +127,7 @@ export default {
       description:
         'Scrollytelling library with the IntersectionObserver in React.',
       url: 'https://jsonkao.github.io/react-scrollama',
+      people: 'Nicholas Yang',
     },
     {
       name: 'Free Code Camp',
@@ -120,12 +135,14 @@ export default {
       description:
         'Contributions to lessons and interactive coding architecture',
       url: 'https://www.columbiaspectator.com/news/net-price-inequity',
+      people: 'Nicholas Yang',
     },
     {
       name: 'GraphQL HTTP client for Go',
       image: 'https://i.imgur.com/4Vz0dPa.png',
       description: '',
       url: 'https://github.com/machinebox/graphql',
+      people: 'Nicholas Yang',
     },
   ],
 };
