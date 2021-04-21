ebd10df179f0240190aaa50b68ff993f65bd8f35
Jason Kao <jason.kao@columbia.edu>
2021-04-20 21:44:10 -0400

    nyt update

--- src/App.svelte
+++ src/App.svelte
@@ -13,10 +13,86 @@
   let contactFocused = false;
 </script>
 
+<main style="max-width: {MAX_WIDTH}px">
+  <header>
+    <div>
+      <p>Jason Kao</p>
+    </div>
+    <div>
+      <p>About.</p>
+      <p class="gray">
+        <button on:click={() => (contactFocused = !contactFocused)}>
+          Contact.
+        </button>
+      </p>
+    </div>
+  </header>
+
+  <p class="nutgraf">
+    I am an Asian-American journalist hoping to bring together different mediums
+    and methods. I am a reporter on the New York Times Graphics desk.
+  </p>
+
+  <div class="nutgraf contact" class:contactFocused>
+    <p>
+      <b>Contact:</b>
+      <a href="mailto:jason.kao@nytimes.com}">jason.kao@nytimes.com</a>
+      <a href="https://twitter.com/jsonkao">@jsonkao</a>
+      <a href="mailto:{email}">{email}</a>
+      <a href="https://github.com/jsonkao">github/jsonkao</a>
+    </p>
+  </div>
+
+  <Grid projects={projects.topLevel} />
+
+  <SectionHeader id="school" centered>Class projects</SectionHeader>
+  <Grid projects={projects.school} />
+
+  <SectionHeader id="extended-portfolio" centered>
+    Misc.; <a href="https://www.github.com/jsonkao">Open source</a> software
+    <!--
+    More work at <a
+      href="https://www.bloomberg.com/authors/AUy6CJsqwxM/jason-kao"
+    >Bloomberg News</a>, <a href="https://www.nytimes.com/by/jason-kao">The New
+      York Times</a> and <a href="https://www.columbiaspectator.com/contributors/Jason-Kao"> the
+      Columbia Spectator </a>
+    -->
+  </SectionHeader>
+  <Grid projects={projects.secondLevel} />
+
+  <footer class="skinny">
+    <SectionHeader>Buzzwords</SectionHeader>
+    <div class="buzzwords">
+      <p>
+        Data and spatial analysis with Python, R, SQL, QGIS, mapshaper, and AWS.
+      </p>
+      <p>
+        Visualizations and web stuff with JavaScript, D3, WebGL, Svelte, Node, <code
+          >...frameworks</code
+        > , GraphQL, and TypeScript.
+      </p>
+      <p>
+        Satellite imagery processing with GDAL, Python, Google Earth Engine and
+        Google Cloud Platform.
+      </p>
+      <p>Design with Illustrator and Photoshop.</p>
+    </div>
+
+    <SectionHeader empty />
+    <p class="updated-text">
+      Updated April 2021. <span style="text-transform: none;"
+        ><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"
+          >Creative Commons CC BY-SA 4.0</a
+        >.</span
+      >
+    </p>
+  </footer>
+</main>
+
 <style lang="scss">
   main {
     margin: 0 auto;
-    padding: 0 40px;
+    padding: 0 30px;
   }
 
   header {
@@ -117,86 +193,13 @@
     font-size: 16px;
     font-weight: 500;
     line-height: 24px;
-    color: #aaa;
+    color: #999;
   }
 
   @media (max-width: 600px) {
-    header {
-      margin-bottom: 48px;
-    }
-
     .nutgraf,
     .skinny {
       width: 100%;
     }
   }
 </style>
-
-<main style="max-width: {MAX_WIDTH}px">
-  <header>
-    <div>
-      <p>Jason Kao</p>
-    </div>
-    <div>
-      <p>About.</p>
-      <p class="gray">
-        <button on:click={() => (contactFocused = !contactFocused)}>
-          Contact.
-        </button>
-      </p>
-    </div>
-  </header>
-
-  <p class="nutgraf">
-    I am an Asian-American journalist hoping to bring together different mediums and methods. I am a junior at Columbia.
-  </p>
-
-  <div class="nutgraf contact" class:contactFocused>
-    <p>
-      <b>Contact:</b>
-      <a href="mailto:{email}">{email}</a>
-      <a href="https://github.com/jsonkao">github/jsonkao</a>
-      <a href="https://twitter.com/jsonkao">@jsonkao</a>
-    </p>
-  </div>
-
-  <Grid projects={projects.topLevel} />
-
-  <SectionHeader id="school" centered>
-    Class projects
-  </SectionHeader>
-  <Grid projects={projects.school} />
-
-  <SectionHeader id="extended-portfolio" centered>
-    Misc.; <a href="https://www.github.com/jsonkao">Open source</a> software
-    <!--
-    More work at <a
-      href="https://www.bloomberg.com/authors/AUy6CJsqwxM/jason-kao"
-    >Bloomberg News</a>, <a href="https://www.nytimes.com/by/jason-kao">The New
-      York Times</a> and <a href="https://www.columbiaspectator.com/contributors/Jason-Kao"> the
-      Columbia Spectator </a>
-    -->
-  </SectionHeader>
-  <Grid projects={projects.secondLevel}/>
-
-  <footer class="skinny">
-    <SectionHeader>Buzzwords</SectionHeader>
-    <div class="buzzwords">
-      <p>Data and spatial analysis with Python, R, SQL, QGIS, mapshaper, and AWS.</p>
-      <p>
-        Visualizations and web stuff with JavaScript, D3, WebGL, Svelte, Node, <code
-        >...frameworks</code> , GraphQL, and TypeScript.
-      </p>
-      <p>Satellite imagery processing with GDAL, Python, Google Earth Engine and Google Cloud Platform.</p>
-      <p>Design with Illustrator and Photoshop.</p>
-    </div>
-
-    <SectionHeader empty />
-    <p class="updated-text">Updated March 2021. <span style="text-transform: none;">
-      All are welcome to use this <a href="https://github.com/jsonkao/jasonkao.me">code/design</a> under the <a
-        rel="license"
-        href="http://creativecommons.org/licenses/by-sa/4.0/"
-      >Creative Commons Attribution-ShareAlike 4.0 International License</a>.</span>
-    </p>
-  </footer>
-</main>
